ff1212af8c85304c8ea671d39da9f58c27dd425d
keyva <keyvanmatinrad@outlook.com>
2025-06-12 22:22:02 +0330

    finalized api calls

--- app/lib/api/insurance.ts
+++ app/lib/api/insurance.ts
@@ -1,11 +1,35 @@
 import request from '@/app/lib/api/client';
 import { DynamicForm } from '@/app/lib/types/dynamic-form';
+import { TableData } from '@/app/lib/types/table-data';
 
 export const fetchDynamicForm = async () => {
-  const response = await request<DynamicForm>('insurance/forms');
+  const response = await request<DynamicForm[]>('/api/insurance/forms');
 
   if (!response) {
-    throw new Error('خطایی در دریافت لیست بلاگ‌ها رخ داده است');
+    throw new Error('خطایی در دریافت لیست فرم های بیمه رخ داده است');
+  }
+
+  return response;
+};
+
+export const submitForm = async (data: Partial<DynamicForm>) => {
+  const response = await request<DynamicForm>('/api/insurance/forms/submit', {
+    method: 'POST',
+    body: JSON.stringify(data),
+  });
+
+  if (!response) {
+    throw new Error('خطایی در ثبت درخواست بیمه رخ داده است');
+  }
+
+  return response;
+};
+
+export const fetchInsuranceSubmissions = async () => {
+  const response = await request<TableData>('/api/insurance/forms/submissions');
+
+  if (!response) {
+    throw new Error('خطایی در دریافت لیست درخواست های بیمه رخ داده است');
   }
 
   return response;
